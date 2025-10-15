63f4d3391f90e67743bff5130b9cb473446f85c3
azharalsaadi <azharalsaadi1999@gmail.com>
2025-10-15 16:50:28 +0400

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_oriented_programming.Models
+{
+    public class Employee
+    {
+        public const double TAX = 0.03;
+
+        public int Id;
+        public string Fname;
+        public string Lname;
+        public decimal Wage;
+        public double LoggedHours;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace object_oriented_programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] emp = new Employee[3];
+            for (int i = 0; i < emp.Length; i++)
+            {
+                emp[i] = new Employee();
+                Console.WriteLine($"please enter emp {i} First Name :");
+                emp[i].Fname = Console.ReadLine();
+
+
+                Console.WriteLine($"please enter emp {i} Last Name :");
+                emp[i].Lname = Console.ReadLine();
+
+                Console.WriteLine($"please enter emp {i} Wage (Rate per hour) :");
+                emp[i].Wage = Decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine($"please enter emp {i} Logged Hours (Working hour) :");
+                emp[i].LoggedHours = double.Parse(Console.ReadLine());
+            }
+            foreach (Employee item in emp)
+            {
+                decimal NetSalary = (item.Wage * (decimal)item.LoggedHours) - (item.Wage * (decimal)(item.LoggedHours * Employee.TAX));
+                Console.WriteLine($"Employee {item.Fname} : Net Salary is {NetSalary}");
+
+            }
+        }
+    }
+}
